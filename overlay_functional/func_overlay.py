7c38f4129438b78b127c59576ebf33b6237828d0
miki998 <miki998chan@gmail.com>
2023-07-27 10:27:06 +0200

    removed cv2 import and hardcode path

--- overlay_functional/func_overlay.py
+++ overlay_functional/func_overlay.py
@@ -1,20 +1,17 @@
-import numpy as np
-import vtk
 import wx
+import vtk
+import numpy as np
 from pubsub import pub as Publisher
 from six import with_metaclass
 
 from invesalius.data import styles
 
-# erosion and dilation related
-import cv2
 import nibabel as nib
 from copy import deepcopy
-import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 from sklearn.cluster import KMeans
 
-
+import os
 from . import gui
 
 class FunctionalOverlayStyle(styles.DefaultInteractorStyle):
@@ -22,12 +19,15 @@ class FunctionalOverlayStyle(styles.DefaultInteractorStyle):
     def __init__(self, viewer):
         super().__init__(viewer)
 
-        # NOTE: Currently we load pre-computed files here
-        self.datapath = '/home/chunhei/Desktop/projects/gsoc/resources/'
+        self.picker = vtk.vtkWorldPointPicker()
+        # NOTE: BIDS folder pathing
+        # TODO: Selecting path instead - may be replaced by navigation system button option
+        self.datapath = '../resources/'
         self.func_frame = nib.load(self.datapath + 'sing_func_subj.nii.gz').get_fdata()
         self.func_conn = nib.load(self.datapath + 'G1_func_subspace.nii.gz').get_fdata()
         self.yeo7 = nib.load(self.datapath + 'yeo7vol_subspace.nii.gz').get_fdata()
         self.yeo17 = nib.load(self.datapath + 'yeo17vol_subspace.nii.gz').get_fdata()
+
 
         self.cluster_smoothness = 10
         self.AddObserver("LeftButtonPressEvent", self.OnPressLeftButtonOverlay)
@@ -43,16 +43,45 @@ class FunctionalOverlayStyle(styles.DefaultInteractorStyle):
         if self.gui is not None:
             self.destroy_gui()
 
+    def _SeedbasedCompute(self):
+        pass
+
+    def _ClusterCompute(self):
+        pass
+
     def OnPressLeftButtonOverlay(self, obj, evt):
+        mask = self.viewer.slice_.current_mask
+        print(os.getcwd())
 
         # Set the overlay
         # we transpose and flip since the functional frames are not rotated in the same way structurals are
         if self.gui.choice_morph == "TimeFrame":
-            # NOTE: not done implemented
             tmp = deepcopy(self.func_frame).T[:,::-1]
             # -> Idea is to generate one hue but with different intensity for the whole volume, intensity depending on the BOLD value
+            # 0. Clustering of colors and values from tmp
+            # - Arbitrary clustering (for now) for Functional-Connectivity option
+            tmp[np.isnan(tmp)] = -10000 # artificially cluster together the nans
+
+            kmeans = KMeans(init="k-means++", n_clusters=self.cluster_smoothness, n_init=4, random_state=0)
+            res = kmeans.fit_predict(tmp.flatten().reshape(-1,1))
+            # convert the clustered regions back to volume shape
+            clust_vol = res.reshape(tmp.shape)
+
+            # 1. Create layer of shape first
+            self.viewer.slice_.aux_matrices['color_overlay'] = clust_vol
+            self.viewer.slice_.aux_matrices['color_overlay'] = self.viewer.slice_.aux_matrices['color_overlay'].astype(int)
+
+            # 2. Attribute different hue accordingly
+            color = cm.rainbow(np.linspace(0, 1, self.cluster_smoothness), alpha=0.4)
+            self.viewer.slice_.aux_matrices_colours['color_overlay'] = {k+1: (1.0,1.0,0.0, 1/(k+1)) for k in range(self.cluster_smoothness)}
+            self.viewer.slice_.aux_matrices_colours['color_overlay'][0] = (0.0, 0.0, 0.0, 0.0) # add transparent color for nans and non GM voxels
+
+            # 3. Show colors
+            self.viewer.slice_.to_show_aux = 'color_overlay'
+            self.viewer.discard_mask_cache(all_orientations=True, vtk_cache=True)
+
+            Publisher.sendMessage('Reload actual slice')
             return
-        
         elif self.gui.choice_morph == "Yeo-7":
             self.cluster_smoothness = 7
             tmp = deepcopy(self.yeo7)
